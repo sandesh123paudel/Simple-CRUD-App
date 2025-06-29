2e7e6e6784d9152c12ddd7549cf530c9d7cb96a0
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-06-29 15:51:36 +0545

    Refactor app structure to integrate MongoDB and session management. Updated dependencies in package.json and package-lock.json. Changed main entry point to app.js.

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
+//imports
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const router = require("./routes/routes");
 
+//app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port  http://localhost:${PORT}`);
-});
+// set template engine
+app.set("view engine", "ejs");
+
+//route prefix
+app.use("", router);
+
+const PORT = process.env.PORT || 4000;
+const DB_PATH = process.env.DB_PATH;
+
+//connect to mongo and start server
+mongoose
+  .connect(DB_PATH)
+  .then(() => {
+    console.log("Connected to Mongo");
+    app.listen(PORT, () => {
+      console.log(`Server running on address: http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error while connecting to Mongo", err);
+  });
